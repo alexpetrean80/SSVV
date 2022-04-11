6fb3cc8ffdde06ed0a4879990b55c6ccf5cfea53
Alex Petrean <alex.petrean@outlook.com>
2022-04-11 08:16:18 +0300

    added integration test template

---
+++ src/test/java/service/IntegrationTest.java
@@ -0,0 +1,23 @@
+package service;
+
+public class IntegrationTest {
+  @Test
+  public void addStudent_test() {
+
+  }
+
+  @Test
+  public void addAssignment_test() {
+
+  }
+
+  @Test
+  public void addGrade_test() {
+
+  }
+
+  @Test
+  public void addAll_test() {
+    
+  }
+}
